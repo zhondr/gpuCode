950b3dc602d3a09206556ac55d8182e5b33a78c3
zhondr <zhandarbek@gmail.com>
2020-02-27 20:24:34 +0600

    plot in milliseconds and semilog y axis plot.

--- plot.py
+++ plot.py
@@ -8,7 +8,13 @@ gpu = data_file[:,1]
 gpuwithoverhead = data_file[:,2]
 cpu = data_file[:,3]
 
-plt.plot(matrixsize,cpu,matrixsize,gpu,matrixsize,gpuwithoverhead)
+plt.plot(matrixsize,cpu*1000,matrixsize,gpu*1000,matrixsize,gpuwithoverhead*1000)
+plt.legend(['CPU','GPU without copy overhead','GPU with overhead'],loc='best')
+plt.xlabel('matrix size')
+plt.ylabel('time in milliseconds')
+plt.show()
+
+plt.semilogy(matrixsize,cpu,matrixsize,gpu,matrixsize,gpuwithoverhead)
 plt.legend(['CPU','GPU without copy overhead','GPU with overhead'],loc='best')
 plt.xlabel('matrix size')
 plt.ylabel('time')
